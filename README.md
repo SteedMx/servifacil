aad31c99909dbf224d6a547bbd8dd3f61419074b
Michell Ayala <starships@outlook.com>
2016-09-09 12:05:48 -0500

    project config

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Servifacil web site
+
+## Installation
+
+### Node
+You can go to the [oficial web page](https://nodejs.org) and download it or you can use [nvm](https://github.com/creationix/nvm).
+
+### NPM
+The node package manager (`npm`) comes with node, dont worrie.
+
+### Project
+```
+$ git clone git@github.com:SteedMx/servifacil.git
+$ cd servifacil
+$ npm install
+```
+
+## Build
+You need to have installed [gulp](http://gulpjs.com/) in your machine.
+
+### Development (debug)
+```
+$ gulp [build]
+```
+
+Gulp will run the next tasks:
+- `debug:html`: will move `index.html` from `src` directory to `build` directory.
+- `debug:images`: will move all images from `src` directory to `build` directory.
+- `debug:css`: will compile all the stylus files ffom `src` directory and will create `main.css` in `build` directory.
+- `debug:javascript`: will concat all javascript files from `src` and will create `all.js` in `build` directory.
+- `server`: will start a server at `localhost:8080` that will serve `build` directory
+
+### Production
+```
+$ gulp dist
+```
+
+Gulp will run the next tasks:
+- `dist:html`: will move and minify `index.html` from `src` directory to `build` directory.
+- `dist:images`: will move and optimize all images from `src` directory to `build` directory.
+- `dist:css`: will compile and compress all the stylus files ffom `src` directory and will create `main.css` in `build` directory.
+- `dist:javascript`: will concat, compress and uglify all javascript files from `src` and will create `all.js` in `build` directory.
+- `clean`: will delete `src` directory and will move all files and directories from `build` to the root of the project.
+
+### Github
+```
+$ gulp github
+```
+
+Gulp will run the next tasks:
+- `gh:html`: will move and minify `index.html` from `src` directory to `docs` directory.
+- `gh:images`: will move and optimize all images from `src` directory to `docs` directory.
+- `gh:css`: will compile and compress all the stylus files ffom `src` directory and will create `main.css` in `docs` directory.
+- `gh:javascript`: will concat, compress and uglify all javascript files from `src` and will create `all.js` in `docs` directory.
+
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var stylus = require('gulp-stylus');
 var imageop = require('gulp-image-optimization');
 var concat = require('gulp-concat');
 var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+
+/*!
+ * Development tasks
+ */
 
 gulp.task('server', function () {
   gulp
-    .src('./docs')
+    .src('./build')
     .pipe(webserver({
       host: '0.0.0.0',
       port: 8080,
@@ -27,13 +32,87 @@ gulp.task('watch', function () {
   gulp.watch('src/index.html', ['html']);
 });
 
-gulp.task('javascript', function () {
-  return gulp.src('src/javascript/*.js')
+gulp.task('debug:javascript', function () {
+  gulp.src('src/javascript/*.js')
     .pipe(concat('all.js'))
+    .pipe(gulp.dest('build/javascript'));
+});
+
+gulp.task('debug:images', function () {
+  gulp
+    .src(['src/images/*.png'])
+    .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('debug:css', function () {
+  gulp
+    .src('src/styles/main.styl')
+    .pipe(stylus({ compress: false }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('debug:html', function () {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('build'));
+});
+
+/*!
+ * Production tasks
+ */
+
+gulp.task('dist:javascript', function () {
+  gulp.src('src/javascript/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/javascript'));
+});
+
+gulp.task('dist:images', function () {
+  gulp
+    .src(['src/images/*.png'])
+    .pipe(imageop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('dist:css', function () {
+  gulp
+    .src('src/styles/main.styl')
+    .pipe(stylus({ compress: true }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('dist:html', function () {
+  gulp.src('src/index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function () {
+  del.sync('src')
+
+  gulp
+    .src('build/*')
+    .pipe(gulp.dest('./'))
+})
+
+/*!
+ * Github tasks
+ */
+
+gulp.task('gh:javascript', function () {
+  gulp.src('src/javascript/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('docs/javascript'));
 });
 
-gulp.task('images', function () {
+gulp.task('gh:images', function () {
   gulp
     .src(['src/images/*.png'])
     .pipe(imageop({
@@ -44,14 +123,14 @@ gulp.task('images', function () {
     .pipe(gulp.dest('docs/images'));
 });
 
-gulp.task('css', function () {
+gulp.task('gh:css', function () {
   gulp
     .src('src/styles/main.styl')
     .pipe(stylus({ compress: true }))
     .pipe(gulp.dest('docs/css'));
 });
 
-gulp.task('html', function () {
+gulp.task('gh:html', function () {
   gulp.src('src/index.html')
     .pipe(htmlmin({
       collapseWhitespace: true
@@ -59,5 +138,6 @@ gulp.task('html', function () {
     .pipe(gulp.dest('docs'));
 });
 
-gulp.task('default', ['server', 'javascript', 'css', 'html', 'watch']);
-gulp.task('build', ['javascript', 'css', 'html']);
+gulp.task('default', ['server', 'debug:javascript', 'debug:css', 'debug:html', 'watch']);
+gulp.task('github', ['gh:javascript', 'gh:images', 'gh:css', 'gh:html'])
+gulp.task('build', ['dist:javascript', 'dist:css', 'dist:html', 'dist:images', 'clean']);
