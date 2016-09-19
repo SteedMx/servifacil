88c4a371843c513bf5c727531f392b8cabf1eab4
Michell Ayala <starships@outlook.com>
2016-09-19 13:33:13 -0500

    add: half orange circle in modals

--- src/javascript/z-carousel.js
+++ src/javascript/z-carousel.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-  $('.mosaic__container').slick({
-    autoplay: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  })
-
  $('.us__sections').slick({
     autoplay: true,
     infinite: true,
