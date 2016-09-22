2863f7fc72cec86e14fa4b0b7601149c0bad7c79
Michell Ayala <starships@outlook.com>
2016-09-21 23:42:04 -0500

    add: server

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const files = () => express.static(resolve(__dirname, 'public'))
 app.use(logger('tiny'))
 app.use(files())
 
-app.get('/', function () {
-  res.sendFile('public/index.html')
+app.get(/|index(.html)?$/, (req, res) => {
+  res.sendFile(resolve(__dirname, 'public/index.html'))
 })
 
-app.get('/metodos', function (req, res) {
-  res.sendFile('public/metodos.html')
+app.get(/metodos(.html)?$/, (req, res) => {
+  res.sendFile(resolve(__dirname, 'public/metodos.html'))
 })
 
 if (!module.parent) {
